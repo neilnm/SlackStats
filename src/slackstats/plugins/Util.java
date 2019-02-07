ab63606577e1a005e2f396365e333c913e8edb66
Arvid Müllern-Aspegren <arvidma@users.noreply.github.com>
2019-02-07 21:53:07 +0100

    Extract buildRankOutput into the plugins.Util.

--- src/slackstats/plugins/Util.java
+++ src/slackstats/plugins/Util.java
@@ -11,4 +11,34 @@ public class Util {
         rankToEmoji.put(2, ":two: ");
         rankToEmoji.put(3, ":three: ");        
     }
+
+    public static String buildRankOutput(Map<String, Integer> scoreMap, Map<String, Integer> historicalScoreMap, String suffix, int listLength) {
+        // Build a top-N list from username+value map with optional diff against historical value
+        // Suffix is e.g "points", "hours" etc, to be suffixed after score value.
+        StringBuilder sb = new StringBuilder();
+
+        int index = 1;
+        for (Map.Entry<String, Integer> entry : scoreMap.entrySet()) {
+            if (index == listLength) {
+                break;
+            }
+            String rank = rankToEmoji.get(index);
+            int total = entry.getValue();
+            sb.append(rank).append("<@").append(entry.getKey()).append("> *")
+                    .append(entry.getValue()).append(" ").append(suffix).append("* ");
+
+            if (historicalScoreMap != null) {
+                int ori = 0;
+                if (historicalScoreMap.get(entry.getKey()) != null) {
+                    ori = historicalScoreMap.get(entry.getKey());
+                }
+                sb.append("%2B").append(total - ori);
+            }
+            
+            sb.append("\n");
+            index++;
+        }
+        return sb.toString();
+    }
+
 }
