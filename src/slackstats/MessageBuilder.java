f927acf89441db19553e601c987873b59b6aa580
Neil Mann <neilnm@gmail.com>
2021-02-27 22:07:31 -0500

    Removed Terminators and Cricket plugins

--- src/slackstats/MessageBuilder.java
+++ src/slackstats/MessageBuilder.java
@@ -3,19 +3,15 @@ package slackstats;
 import slackstats.plugins.Babblers;
 import slackstats.plugins.Plugin;
 import slackstats.plugins.WordCounter;
-import slackstats.plugins.TerminatorSummoner;
 import slackstats.plugins.ReactionCounter;
-import slackstats.plugins.Cricketeering;
 
 public class MessageBuilder {
 
     SlackReader comm;
-    boolean useTermSum;
     String fromChannel;
 
     MessageBuilder(Settings settings, String channel) {
         comm = new SlackReader(settings, channel);
-        useTermSum = settings.channelTermSumDo.get(channel);
         fromChannel = channel;
     }
 
@@ -27,11 +23,9 @@ public class MessageBuilder {
         slackMsg.append("<#").append(fromChannel)
                 .append("> Weekly Stats by <@neil.mancini>\n");
 
-        Plugin termsum = new TerminatorSummoner(comm);
         Plugin wordcounter = new WordCounter();
         Plugin babblers = new Babblers(comm);
         Plugin reactions = new ReactionCounter(comm);
-        Plugin cricketeering = new Cricketeering();
 
         for (SlackData sd : comm.getLastWeekData()) {
             for (int i = 0; i < sd.messages.length(); i++) {
@@ -39,17 +33,12 @@ public class MessageBuilder {
                 wordcounter.update(jMsg);
                 babblers.update(jMsg);
                 reactions.update(jMsg);
-                cricketeering.update(jMsg);
-                if (useTermSum) termsum.update(jMsg);
-
             }
         }
 
-        if (useTermSum) slackMsg.append(termsum.buildOutput());
         slackMsg.append(wordcounter.buildOutput());
         slackMsg.append(babblers.buildOutput());
         slackMsg.append(reactions.buildOutput());
-        slackMsg.append(cricketeering.buildOutput());
 
         slackMsg.append("\n Source Code: https://github.com/neilnm/SlackStats");
 
